92bce9c1e59c48c7fd3cda93fe700ceeb22f01d9
Ayça Akdemir <mabeaaa@gmail.com>
2021-04-20 14:38:28 +0300

    20.04

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+    }
+}
